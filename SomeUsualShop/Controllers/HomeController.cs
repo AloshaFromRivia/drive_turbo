99a30aee95a8c37e0be5f64bd304c31d4890abbf
AloshaFromRivia <alex-iv999.ivanov@yandex.ru>
2022-04-13 01:06:22 +0300

    Creating project

---
+++ SomeUsualShop/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SomeUsualShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult About() => View();
+        public ViewResult Reviews()=> View();
+    }
+}
